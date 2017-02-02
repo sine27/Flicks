7aa060169616ff40b94b92464c4349f91b0014fd
sine27 <feng87@purdue.edu>
2017-02-02 16:27:49 -0500

    search activated

--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -9,13 +9,23 @@
 import UIKit
 import AFNetworking
 
-class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
     @IBOutlet weak var moviesTableView: UITableView!
     
+    @IBOutlet weak var mySearchBar: UISearchBar!
+    
+    // All movies info from database
     var movies: [NSDictionary] = []
     
+    // filtered movies info after seach bar activated
+    var searchResults : [NSDictionary] = []
+    
+    // pull to refresh
     let refreshControl = UIRefreshControl()
+    
+    // if seach bar is activated
+    var searchActive = false
     
     // MARK : Activity indicator >>>>>
     fileprivate var blur = UIVisualEffectView(effect: UIBlurEffect(style: UIBlurEffectStyle.dark))
@@ -43,10 +53,19 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
         blur.removeFromSuperview()
     }
     
+    // <<<<<< Activity indicator
+    
+    // re-send the request
     func refresh(sender:AnyObject) {
         request()
     }
     
+    // hide keyboard by gesture
+    @IBAction func onTap(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    // url request for data
     func request () {
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
@@ -55,8 +74,7 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
         let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
             if let data = data {
                 if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
-                    // print(dataDictionary)
-                    
+
                     self.movies = dataDictionary["results"] as! [NSDictionary]
                     
                     self.moviesTableView.reloadData()
@@ -73,18 +91,20 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // display activity indicator
         activityIndicator()
         
+        // setup pull to refresh activity indicator
         refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
         refreshControl.addTarget(self, action: #selector(MoviesViewController.refresh(sender: )), for: UIControlEvents.valueChanged)
         moviesTableView.addSubview(refreshControl)
 
+        // tableview setup
         moviesTableView.delegate = self
         moviesTableView.dataSource = self
         moviesTableView.rowHeight = 240
 
-        // Do any additional setup after loading the view.
-        
+        // requst for data
         request()
     }
 
@@ -93,21 +113,33 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
         // Dispose of any resources that can be recreated.
     }
     
+    // return number of rows
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if(searchActive) {
+            return searchResults.count
+        }
         return movies.count
     }
     
+    // retrun data for each cell
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell") as! MovieCell
-
-        let movie = movies[indexPath.row]
         
-        // let timestamp = movie["timestamp"] as? String
+        let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell") as! MovieCell
+        
+        var movie : NSDictionary
+        
+        // check if searching
+        if(searchActive){
+            movie = searchResults[indexPath.row]
+        } else {
+            movie = movies[indexPath.row]
+        }
+        
+        // assign data
         if let imageUrlString = movie.value(forKeyPath: "poster_path") as? String {
             let newImageUrlString = "https://image.tmdb.org/t/p/w342\(imageUrlString)"
             let imageUrl = URL(string: newImageUrlString)!
             cell.movieImage.setImageWith(imageUrl)
-            // print(movie)
             let title = movie.value(forKeyPath: "title") as? String
             cell.movieTitle.text = title
             
@@ -116,8 +148,49 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
             let detail = movie.value(forKeyPath: "overview") as! String
             cell.movieDetail.text = detail
         }
-        
         return cell
     }
+    
+    // MARK : search bar controller >>>>>
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchActive = true;
+        self.moviesTableView.reloadData()
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchActive = false;
+        mySearchBar.text = ""
+        mySearchBar.resignFirstResponder()
+        self.moviesTableView.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        mySearchBar.resignFirstResponder()
+        searchActive = false;
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        // reset search result
+        searchResults = []
+        
+        // search filter
+        for movie in movies {
+            let title: String = movie.value(forKey: "title") as! String
+            if title.range(of: searchText, options: NSString.CompareOptions.caseInsensitive) != nil {
+                searchResults.append(movie)
+            }
+        }
+        
+        // reload data for search
+        searchActive = true;
+        self.moviesTableView.reloadData()
+    }
+    // <<<<< search bar controller
+    
 
 }
